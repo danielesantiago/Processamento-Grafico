f707d46e62606327b1590734c676f3e12751811a
Natalia Carvalho <natalia.carvalho@dcomp.sor.ufscar.br>
2022-09-29 17:26:36 -0300

    criando cena e adicionando primeiro elemento

--- js/projeto2.js
+++ js/projeto2.js
@@ -42,3 +42,35 @@ const _FS2 = `
         gl_FragColor = vec4(mix(colorA, colorB, vUv.z), 1.0);
       }
   `;
+
+  // Criação da cena
+const scene = new THREE.Scene();
+// Criação da câmera
+const camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 1, 1000);
+// Posicionamento da camera
+camera.position.z = 60;
+
+// Renderização
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+// Colocando background de estrelinhas
+const loader = new THREE.TextureLoader();
+loader.load('https://images.pexels.com/photos/1205301/pexels-photo-1205301.jpeg' , function(texture)
+            {
+             scene.background = texture;  
+            });
+
+// Adicionando luz à cena
+const light = new THREE.PointLight( 0xffffff, 1, 100 );
+light.position.set( -20, 15, 50 );
+scene.add( light );
+
+
+// Criando o nó de torus
+const torusKnotGeometry = new THREE.TorusKnotGeometry( 10, 3, 100, 16 );
+const torusKnotMaterial = new THREE.MeshPhongMaterial( { color: 0xC70039  } );
+const torusKnot = new THREE.Mesh( torusKnotGeometry, torusKnotMaterial );
+torusKnot.rotation.set(0.3, 2.9, 0);
+scene.add( torusKnot );
